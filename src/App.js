b77620bc1179989f46629f5c62e2141e3ccb30f9
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2023-12-06 17:13:58 +0100

    rewrote the handle submit function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import "./App.css";
 import React, { useState } from "react";
 
- export default function App() {
+export default function App() {
 
-  const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
+  const [newItem, setNewItem] = useState("")
 
 
+  function handleSubmit(e) {
+
+    e.preventDefault()
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+    setNewItem("")
+  }
 
 
-function handleSubmit(e) {
-  e.preventDefault()
-  setTodos([
-    ...todos, {id: crypto.randomUUID, title: newItem, completed: false},
-  ])
-}
-
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) return { ...todo, completed }
+      })
+    })
+  }
 
   return (
     <>
       <form>
-        <label htmlFor="addTodo">
-          Add New Item
-          <input type="text" id="addTodo" value={newItem} onChange={e => setNewItem(e.target.value)} />
-        </label>
-        <button type="submit" onClick={handleSubmit}>Add Item</button>
-      </form>
-
-      <ul>
-        {todos.map(todo => {
-          return(
-          <li key={todo.id}>
-            <label>
-              <input type="checkbox" checked={todo.completed} />
-              {todo.title}
-            </label>
-          </li>)
-        })}
-      </ul>
+        <label htmlFor="newItemInput">New Item </label>
+        <input type="text" id="newItemInput" placeholder="Enter new Todo here" value={newItem}
+          onChange={e => setNewItem(e.target.value)} />
+        <button type="submit" onClick={handleSubmit} >
+          Add
+        </button>
+        <ul>
+          {todos.map(todo => {
+            return (
+              <li key={todo.id}>
+                <label>
+                  <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                  {todo.title}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </form >
     </>
-  );}
+  );
+}
