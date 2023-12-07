c33e7a36b13d8683ca7b805e0517333b16ec0596
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2023-12-07 23:09:30 +0100

    practiced the toggleTodo function and handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
 
-  const [todos, setTodos] = useState([])
-  const [newItem, setNewItem] = useState("")
+    const [newItem, setNewItem] = useState("")
+    const [todos, setTodos] = useState([])
 
 
-  function handleSubmit(e) {
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTodos(currentTodos => {
+            return [
+                ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
+            ]
+        })
+        setNewItem("")
+    }
 
-    e.preventDefault()
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
-      ]
-    })
-    setNewItem("")
-  }
+    function toggleTodo(id, completed) {
+        setTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if (todo.id === id) return { ...todo, completed }
+            })
+        })
+    }
 
-
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) return { ...todo, completed }
-      })
-    })
-  }
-
-  return (
-    <>
-      <form>
-        <label htmlFor="newItemInput">New Item </label>
-        <input type="text" id="newItemInput" placeholder="Enter new Todo here" value={newItem}
-          onChange={e => setNewItem(e.target.value)} />
-        <button type="submit" onClick={handleSubmit} >
-          Add
-        </button>
-        <ul>
-          {todos.map(todo => {
-            return (
-              <li key={todo.id}>
-                <label>
-                  <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-                  {todo.title}
+    return (
+        <>
+            <form>
+                <label htmlFor="todoItem">
+                    New Item
+                    <input type="text" id="todoItem" value={newItem} onChange={e => setNewItem(e.target.value)} />
                 </label>
-              </li>
-            )
-          })}
-        </ul>
-      </form >
-    </>
-  );
+                <button type="submit" onClick={handleSubmit} >Add New Item</button>
+            </form>
+            <ul>
+                {todos.map(todo => {
+                    return (
+                        <li key={todo.id}>
+                            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                            {todo.title}
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    );
 }
---
+++ src/App3.js
@@ -0,0 +1,70 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
+  const [newItem, setNewItem] = useState("")
+
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function handleSubmit(e) {
+
+    e.preventDefault()
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+    setNewItem("")
+  }
+
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) return { ...todo, completed }
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <form>
+        <label htmlFor="newItemInput">New Item </label>
+        <input type="text" id="newItemInput" placeholder="Enter new Todo here" value={newItem}
+          onChange={e => setNewItem(e.target.value)} />
+        <button type="submit" onClick={handleSubmit} >
+          Add
+        </button>
+        <ul>
+          {todos.length === 0 && "No Todos"}
+          {todos.map(todo => {
+            return (
+              <li key={todo.id}>
+                <label>
+                  <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                  {todo.title}
+                </label>
+                <button onClick={() => deleteTodo(todo.id)} >Delete</button>
+              </li>
+            )
+          })}
+        </ul>
+      </form >
+    </>
+  );
+}
